cc08620daee94a9221c32427e88a46666d95fe5a
ulong2 <shlchen@whu.edu.cn>
2024-04-09 15:34:26 +0800

    add color

--- src/include/veta/landmark.h
+++ src/include/veta/landmark.h
@@ -6,18 +6,25 @@
 #define VETA_LANDMARK_H
 
 #include <utility>
-
 #include "type_def.hpp"
 
 namespace ns_veta {
     // Define 3D-2D tracking data: 3D landmark with its 2D observations
     struct Observation {
-        Observation() : featId(UndefinedIndexT) {}
+    public:
+        // red, green, blue
+        typedef Eigen::Matrix<uint8_t, 3, 1> Color;
 
-        Observation(Vec2d p, IndexT idFeat) : x(std::move(p)), featId(idFeat) {}
+    public:
+        Observation() : featId(UndefinedIndexT), x(Vec2d::Zero()), color(Color::Zero()) {}
+
+        Observation(Vec2d p, IndexT idFeat, Color c = Color::Zero())
+                : x(std::move(p)), featId(idFeat), color(std::move(c)) {}
 
         Vec2d x;
         IndexT featId;
+        // default: black
+        Color color;
 
         // Serialization
         template<class Archive>
@@ -25,6 +32,8 @@ namespace ns_veta {
             ar(cereal::make_nvp("feat_id", featId));
             const std::vector<double> pp{x(0), x(1)};
             ar(cereal::make_nvp("x", pp));
+            const std::vector<uint8_t> c{color(0), color(1), color(2)};
+            ar(cereal::make_nvp("color", c));
         }
 
         // Serialization
@@ -34,6 +43,9 @@ namespace ns_veta {
             std::vector<double> p(2);
             ar(cereal::make_nvp("x", p));
             x = Eigen::Map<const Vec2d>(&p[0]);
+            std::vector<uint8_t> c(3);
+            ar(cereal::make_nvp("color", c));
+            color = Eigen::Map<const Color>(&c[0]);
         }
     };
 
